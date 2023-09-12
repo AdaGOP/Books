986525e49ac42ae967f1410b7fde544deee0326b
Khoirunnisa' Rizky Noor Fatimah <49296015+khoirunnisarizky@users.noreply.github.com>
2023-09-12 11:35:42 +0700

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by khoirunnisa' rizky noor fatimah on 12/09/23.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Books/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Books
+//
+//  Created by khoirunnisa' rizky noor fatimah on 12/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
